c43b2bad849e1e4245093163b0be46b0a78d6b8c
Yura Manucharyan <mnuch95@mail.ru>
2020-02-01 16:03:52 +0400

    Implement Board

---
+++ src/board/cube.js
@@ -0,0 +1,42 @@
+export class Cube extends Phaser.GameObjects.Container {
+  constructor(scene) {
+    super(scene);
+    this._value = NaN;
+
+    this._build();
+  }
+
+  get value() {
+    return _value;
+  }
+
+  updateValue(value) {
+    this._value = value;
+    //update colour
+  }
+
+  _build() {
+    this._buildBg();
+    this._buildText();
+  }
+
+  _buildBg() {
+    const bg = this.scene.add.graphics();
+    bg.fillStyle(0x33f422);
+    bg.fillRoundedRect(0, 0, 50, 50, 5);
+    this.add(bg);
+    this._bg = bg;
+  }
+
+  _buildText() {
+    // const { width, height } = this._bg;
+
+    const text = this.scene.add.text(25, 25, "0", {
+      fontFamily: '"Arial Black"',
+      color: "black",
+      fontSize: 15
+    });
+    text.setOrigin(0.5);
+    this.add(text);
+  }
+}
